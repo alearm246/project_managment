eab7da20eaec6ea10f605d37f800255e77a9c84a
alearm <alearm246@gmail.com>
2021-07-05 19:53:03 -0500

    create basic login page

--- app/src/components/modules/Login/Login.js
+++ app/src/components/modules/Login/Login.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Login.module.css";
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("username: ", username);
+        console.log("password: ", password);
+    }
+
     return (
-        <div>
-            <h1 className={style.loginHeader}>Login</h1>
+        <div className={style.loginPageHolder}>
+            <div className={style.loginPopup}>
+                <form className={style.loginForm}>
+                    <label>username</label>
+                    <input 
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        className={style.usernameInput}
+                    />
+                    <br />
+                    <label>password</label>
+                    <input 
+                        type="text"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className={style.passwordInput}
+                    />
+                    <button onClick={handleSubmit}>login</button>
+                </form>
+            </div>
         </div>
     )
 }
